cb651374c4e925b44721a5be042a3c6134c2b877
Prak <100105525+prak132@users.noreply.github.com>
2022-09-30 19:32:56 -0700

    modified:New buttons adding functionality   game.java

--- game.java
+++ game.java
@@ -6,16 +6,23 @@ public class game implements ActionListener {
     private int clicks = 0;
     private JLabel label = new JLabel("Number of clicks:  0");
     private JFrame frame = new JFrame();
+    private JPanel panel = new JPanel();
 
     public game() {
 
         JButton button = new JButton("Click Me");
         button.addActionListener(this);
 
-        JPanel panel = new JPanel();
+        JButton upgrade = new JButton("UPGRADE (Requires 400 clicks)");
+
+        
+
+        button.addActionListener(this);
+
         panel.setBorder(BorderFactory.createEmptyBorder(30, 30, 10, 30));
-        panel.setLayout(new GridLayout(0, 1));
+        panel.setLayout(new GridLayout(0, 2));
         panel.add(button);
+        panel.add(upgrade);
         panel.add(label);
 
         frame.add(panel, BorderLayout.CENTER);
@@ -26,7 +33,12 @@ public class game implements ActionListener {
     }
 
     public void actionPerformed(ActionEvent e) {
-        clicks++;
+        ++clicks;
+        if (clicks == 400){
+            int multi = 2;
+            JButton mutliplyer = new JButton("Multi:  X" + multi);
+            panel.add(mutliplyer);
+        }
         label.setText("Number of clicks:  " + clicks);
     }
 
